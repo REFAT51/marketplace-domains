8bbbb192985450f24879930f39b7f2600b00a05b
REFAT51 <rftnjmaldyn222@gmail.com>
2026-04-30 03:11:52 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Welcome</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
